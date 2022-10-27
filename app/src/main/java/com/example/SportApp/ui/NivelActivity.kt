dc4c59493d66886f2975e09ec6feca6e12b67862
andres <a.salasr@uniandes.edu.co>
2022-10-26 23:07:17 -0500

    cascaron

---
+++ app/src/main/java/com/example/SportApp/ui/NivelActivity.kt
@@ -0,0 +1,45 @@
+package com.example.SportApp.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.SportApp.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class NivelActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigationView: BottomNavigationView;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nivel)
+
+        bottomNavigationView = findViewById(R.id.bottom_navigator);
+        bottomNavigationView.setSelectedItemId(R.id.nivel);
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.calendario -> {
+                    val obja = Intent(this, CalendarioActivity::class.java)
+                    startActivity(obja)
+                    true;
+                }
+                R.id.perfil -> {
+                    val objb = Intent(this, PerfilActivity::class.java)
+                    startActivity(objb)
+                    true;
+                }
+                R.id.entrenar -> {
+                    val objc = Intent(this, EntrenarActivity::class.java)
+                    startActivity(objc)
+                    true;
+                }
+                R.id.servicios -> {
+                    val objd = Intent(this, ServiciosActivity::class.java)
+                    startActivity(objd)
+                    true;
+                }
+            }
+            true
+        }
+    }
+}
