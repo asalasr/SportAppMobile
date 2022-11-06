de12539cfc82135dd0cfab1104dec985af917482
andres <a.salasr@uniandes.edu.co>
2022-11-06 17:30:03 -0500

    Ajuste de servicios

--- app/src/main/java/com/example/sportapp/ui/EventosActivity.kt
+++ app/src/main/java/com/example/sportapp/ui/EventosActivity.kt
@@ -1,11 +1,52 @@
 package com.example.sportapp.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.sportapp.R
+import com.example.sportapp.viewmodels.EventoViewModel
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class EventosActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigationView: BottomNavigationView;
+
+    private lateinit var viewModel: EventoViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_eventos)
+        val titleMenu: String = "EVENTOS"
+        setTitle(titleMenu);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigator);
+        bottomNavigationView.setSelectedItemId(R.id.servicios);
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nivel -> {
+                    val obja = Intent(this, NivelActivity::class.java)
+                    startActivity(obja)
+                    true;
+                }
+                R.id.perfil -> {
+                    val objb = Intent(this, PerfilActivity::class.java)
+                    startActivity(objb)
+                    true;
+                }
+                R.id.entrenar -> {
+                    val objc = Intent(this, EntrenarActivity::class.java)
+                    startActivity(objc)
+                    true;
+                }
+
+                R.id.calendario -> {
+                    val objd = Intent(this, CalendarioActivity::class.java)
+                    startActivity(objd)
+                    true;
+                }
+            }
+            true
+        }
     }
 }
--- app/src/main/java/com/example/sportapp/ui/ServiciosActivity.kt
+++ app/src/main/java/com/example/sportapp/ui/ServiciosActivity.kt
@@ -3,16 +3,34 @@ package com.example.sportapp.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
 import com.example.sportapp.R
+import com.example.sportapp.viewmodels.EntrenamientoViewModel
+import com.example.sportapp.viewmodels.EventoViewModel
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.fragment_evento_listar.*
 
 class ServiciosActivity : AppCompatActivity() {
 
     private lateinit var bottomNavigationView: BottomNavigationView;
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_servicios)
+        val titleMenu: String = "SERVICIOS"
+        setTitle(titleMenu);
+
+        val boton1=findViewById<Button>(R.id.btnEventos)
+
+        boton1.setOnClickListener {
+            val intento1 = Intent(this, EventosActivity::class.java)
+            startActivity(intento1)
+
+        }
 
         bottomNavigationView = findViewById(R.id.bottom_navigator);
         bottomNavigationView.setSelectedItemId(R.id.servicios);
@@ -44,4 +62,5 @@ class ServiciosActivity : AppCompatActivity() {
             true
         }
     }
+
 }
